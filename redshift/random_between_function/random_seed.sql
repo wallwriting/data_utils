8b44a7dfaab640998050e3b3991f9df79af2dda5
wallwriting <hwang.john@gmail.com>
2020-05-26 07:25:54 -0400

    New random_seed.sql file
    Overloads the function to allow passing three or only two parameters

--- redshift/random_between_function/random_seed.sql
+++ redshift/random_between_function/random_seed.sql
@@ -2,17 +2,32 @@ CREATE OR REPLACE function random_seed(lowArg BIGINT, highArg BIGINT, seedArg BI
 RETURNS BIGINT
 volatile 
 AS $$
-	def rndm(lowArg, highArg, seedArg):
-		#imports seed and pseudo random
-		from random import seed
-		from random import random
-		# if no argument passed, set to random
-		if seedArg is none:
-			seedArg = random()
-		else:
-        	seedArg = seedArg
-		seed(seedArg)
-		# generate random number
-		return random() * (highArg-lowArg +1) + lowArg
-	return rndm(lowArg,highArg,seedArg)
+    #imports seed and pseudo random
+    from random import seed
+    from random import random
+    # if no argument passed, set to random
+    if seedArg is None:
+        seedArg = random()
+    else:
+        seedArg = seedArg
+    seed(seedArg)
+    # generate random number
+    return random() * (highArg-lowArg +1) + lowArg
+
+$$ LANGUAGE plpythonu;
+
+
+
+
+/*Overloads the function to allow for usage passing only 2 arguments*/
+CREATE OR REPLACE function random_seed(lowArg BIGINT, highArg BIGINT)
+RETURNS BIGINT
+volatile 
+AS $$
+    #imports seed and pseudo random
+    from random import seed
+    from random import random
+    seed(random())
+    # generate random number
+    return random() * (highArg-lowArg +1) + lowArg
 $$ LANGUAGE plpythonu;
