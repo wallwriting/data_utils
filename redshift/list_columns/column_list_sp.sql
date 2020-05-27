0f1bd02d844fc9e4a81e50a7b06368046bda0852
wallwriting <hwang.john@gmail.com>
2020-05-27 00:38:20 -0400

    Update column_list_sp.sql
    change proc name to better match standards
    included a sort, now sorts by ordinal position in the table
    included a suffix for the temp table

--- redshift/list_columns/column_list_sp.sql
+++ redshift/list_columns/column_list_sp.sql
@@ -1,35 +1,40 @@
-CREATE OR REPLACE PROCEDURE column_list_sp(tblName IN CHARACTER VARYING)
+CREATE OR REPLACE PROCEDURE sp_column_list(tblName IN CHARACTER VARYING)
+--DROP PROCEDURE column_list_sp(tblName IN CHARACTER VARYING)
 AS $$
 DECLARE
-    col_nm_cursor record;
-    col_txt_var varchar(100);
+    col_nm_cursor RECORD;
+    col_txt_var CHARACTER VARYING;
 
 BEGIN
-    drop table if exists test_sp_table;
-    create table test_sp_table
-        (test_col varchar(2500));
-    insert into test_sp_table select '';
+    /*Creates the temp table that will hold the column list. Arbitrary table suffix is hard coded
+    because parameterizing would require more queries run as EXECUTE commands with quotes*/
+    DROP TABLE IF EXISTS test_sp_table_043q97tyw674tgr9hj8uefijqw9dp;
+    CREATE TEMP TABLE test_sp_table_043q97tyw674tgr9hj8uefijqw9dp
+        (test_col VARCHAR(2500));
+    INSERT INTO test_sp_table_043q97tyw674tgr9hj8uefijqw9dp SELECT '';
 
     FOR col_nm_cursor IN 
                     /*gets column list from the system table*/
                     SELECT
-                        cast(column_name as varchar(250)) as column_name
-                    from 
+                        CAST(column_name AS VARCHAR(250)) AS column_name
+                    FROM
                         information_schema.columns 
                     WHERE 
                         table_name = tblName
+                    ORDER BY
+                        ordinal_position
         LOOP
           /*converts the cursor to a string*/
           col_txt_var = col_nm_cursor;
         
-          update test_sp_table
-          set test_col = CASE 
+          UPDATE test_sp_table_043q97tyw674tgr9hj8uefijqw9dp
+          SET test_col = CASE 
                               /*skips the blank column name in the first pass*/
                               WHEN test_col = '' 
                                 /*string function removes the leading and trailing parentheses*/
-                                THEN substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
+                                THEN substring(col_txt_var, 2, (SELECT len(col_txt_var) - 2)) 
                                 /*adds a comma in between the values*/
-                                ELSE test_col || ', ' || substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
+                                ELSE test_col || ', ' || substring(col_txt_var, 2, (SELECT len(col_txt_var) - 2)) 
                           END;
 
         END LOOP;
