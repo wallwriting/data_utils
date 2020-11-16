eac0a4a20b5aeb58cecf44f61aa663240ba70d2d
wallwriting <hwang.john@gmail.com>
2020-11-16 15:09:43 -0500

    Create random_timestamp.sql

---
+++ BigQuery/random_data_generator/random_timestamp.sql
@@ -0,0 +1,9 @@
+/*creates random timestamp down to microseconds*/
+CREATE OR REPLACE function demo.random_timestamp(low timestamp ,high timestamp)
+as
+(
+    (
+        SELECT timestamp_micros(CAST(rand() * (UNIX_micros(high) - UNIX_micros(low)) + UNIX_micros(low) as int64))
+    )
+)
+;
