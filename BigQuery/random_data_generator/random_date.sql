86181bef879051e8e8f91565bb4b16b5621e2bca
wallwriting <hwang.john@gmail.com>
2020-11-16 14:45:22 -0500

    Create random_date.sql

---
+++ BigQuery/random_data_generator/random_date.sql
@@ -0,0 +1,9 @@
+/*creates random date*/
+CREATE OR REPLACE function demo.random_date(low date ,high date)
+as
+(
+    (
+        SELECT DATE_FROM_UNIX_DATE(CAST(rand() * (UNIX_DATE(high) - UNIX_DATE(low)) + UNIX_DATE(low) as int64))
+    )
+)
+;
