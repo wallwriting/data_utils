11af445470d0a089a545db8525b88a2bd4b7ef33
wallwriting <hwang.john@gmail.com>
2020-11-16 17:47:26 -0500

    Update random_digits.sql

--- BigQuery/random_data_generator/random_digits.sql
+++ BigQuery/random_data_generator/random_digits.sql
@@ -1,5 +1,5 @@
 /*creates random integer*/
 CREATE OR REPLACE function demo.random_integer(low int64 ,high int64)
 as
-((select cast(rand() * (low - high) + low as int64)))
+((select cast(rand() * (high - low) + low as int64)))
 ;
