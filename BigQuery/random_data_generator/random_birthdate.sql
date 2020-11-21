4e7a6c1d42fb23192ba64c696cb67870c69f2ac2
wallwriting <hwang.john@gmail.com>
2020-11-21 12:09:13 -0500

    Update random_birthdate.sql
    Made it reseedable when generating multiple rows at the same time.

--- BigQuery/random_data_generator/random_birthdate.sql
+++ BigQuery/random_data_generator/random_birthdate.sql
@@ -1,85 +1,70 @@
-/*creates random birthdate weighted by US national average age bands
-the dates passed represent the lowest and highest birthdate years desired*/
-CREATE OR REPLACE function demo.random_birthdate(low1 date, high1 date)
-as
+/*creates random birthdate weighted by US national average age bands.
+Derived ages are based on current year. Min age is 1 and max age is 90*/
+CREATE OR REPLACE FUNCTION test.random_birthdate() AS
 (
     (
-
-        WITH tiers as
-        (
-        /*establishes percentage tiers,
-        taken from US national averages,
-        adjust as desired*/
+        WITH 
+            /*establishes the age band tiers,
+            taken from US national averages.
+            Percntage weights for the bands come later*/
+            bands AS
+            (
+                SELECT 
+                    RAND() AS rnum,
+                    /*1 - 14 yrs old--change this if you want a higher minimum age than 1*/
+                    EXTRACT(YEAR FROM CURRENT_DATE) - CAST(RAND() * (14 - 1) + 1 AS INT64) AS tier1,
+                    /*15-24*/
+                    EXTRACT(YEAR FROM CURRENT_DATE) - CAST(RAND() * (24 - 15) + 15 AS INT64) AS tier2,
+                    /*25 - 54*/
+                    EXTRACT(YEAR FROM CURRENT_DATE) - CAST(RAND() * (54 - 25) + 25 AS INT64) AS tier3,
+                    /*55 - 64*/
+                    EXTRACT(YEAR FROM CURRENT_DATE) - CAST(RAND() * (64 - 55) + 55 AS INT64) AS tier4,
+                    /*64-90--change this if you want a higher max age than 90*/
+                    EXTRACT(YEAR FROM CURRENT_DATE) - CAST(RAND() * (90 - 65) + 65 AS INT64) AS tier5
+            ),
+            /*establishes the percentage weights for each age band.
+            This is taken from US national averages AS of 2020,
+            adjust AS desired*/
+            weights AS
+            (
+                SELECT
+                    (
+                        CASE 
+                            WHEN rnum BETWEEN 0 AND .1862 THEN bands.tier1
+                            WHEN rnum > .1862 AND rnum <= .3174 THEN bands.tier2
+                            WHEN rnum > .3174 AND rnum <= .7103 THEN bands.tier3
+                            WHEN rnum > .7103 AND rnum <= .8397 THEN bands.tier4
+                            ELSE bands.tier5
+                        END
+                    ) yr
+                FROM 
+                    bands
+            )
+        /*starts the main query*/
         SELECT
-        18.62 as tier_1,
-        13.12 as tier_2,
-        39.29 as tier_3,
-        12.94 as tier_4,
-        16.03 as tier_5
-        )
-        ,
-        weight as
-        (
-        /*associates the weights from above to age bands*/
-        select
-        sequence_number as year1,
-        CASE 
-        WHEN sequence_number between 
-                extract(year from current_date) - 14 
-                AND extract(year from current_date)
-                THEN t.tier_1
-        WHEN sequence_number between 
-                extract(year from current_date) - 24
-                AND extract(year from current_date) - 15
-                THEN t.tier_2
-        WHEN sequence_number between 
-                extract(year from current_date) - 54
-                AND extract(year from current_date) - 25
-                THEN t.tier_3
-        WHEN sequence_number between 
-                extract(year from current_date) - 64
-                AND extract(year from current_date) - 55
-                THEN t.tier_4
-        ELSE t.tier_5
-        END as pct
-        from 
---        /*this assumes an age range of between the two explicit numbers below, adjust as desired*/
---        UNNEST(generate_array(extract(year from current_date) - 92, extract(year from current_date) - 13)) as sequence_number
-        UNNEST(generate_array(extract(year from low1), extract(year from high1))) as sequence_number
-        CROSS JOIN
-        tiers t
-        )
-        ,
-        year_as_string AS 
-        (
-        /*repeats the years based on the percentage weight,
-        the higher the weight, the more times the year will
-        show up*/
-        SELECT *,
-        --      extract(year from current_date) - year,
-        CAST(w.year1 as string) yr_string
+            /*Does a leap year check of the random year that wAS generated 
+            and creates a random date for that year*/
+            CASE WHEN
+                    (
+                        (
+                            MOD
+                                (   EXTRACT(YEAR FROM CURRENT_DATE) , 4    ) = 0 
+                            AND 
+                            MOD
+                                (   EXTRACT(YEAR FROM CURRENT_DATE) , 100   ) <> 0
+                        )    
+                        OR
+                            MOD
+                                (   EXTRACT(YEAR FROM CURRENT_DATE) , 400   ) = 0 
+                    /*adds random number of days, between 0 and 365, to Jan 1st if it's a leap year*/
+                    ) IS TRUE THEN CAST(CAST(weights.yr AS STRING) || '-01-01' AS DATE) 
+                                                                            + CAST(RAND() * (365 - 0) + 0 AS INT64) 
+                /*adds random number of days between 0 and 364 if it is not a leap year*/
+                ELSE CAST(CAST(weights.yr AS STRING) || '-01-01' AS DATE)
+                                                + CAST(RAND() * (364 - 0) + 0 AS INT64) 
+                END
         FROM
-        weight w
-        /*creates a partial cross join*/
-        JOIN 
-        UNNEST(generate_array(0,100)) as ms1sequence_number
-        ON ms1sequence_number <= w.pct
-        order by rand()
-        limit 1
-        )
-        SELECT
-                CAST(
-                ys.yr_string || '-'
-                || CAST(EXTRACT(MONTH from bd1.base_date) as string) || '-'
-                || CAST(EXTRACT(DAY FROM bd1.base_date) as string)
-                as date)
-        FROM
-        /*gets a random date in order to extract a random month and day*/
-        (
-                SELECT DATE_FROM_UNIX_DATE(CAST(rand() * (UNIX_DATE('2019-12-31') - UNIX_DATE('2019-01-01')) + UNIX_DATE('2019-01-01') as int64)) as base_date
-        ) as bd1
-        CROSS JOIN
-        year_as_string ys
+            weights
     )
 )
 ;
