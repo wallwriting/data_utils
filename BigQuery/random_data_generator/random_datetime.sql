c3a77fbe118aa9dc4817e41b01075f19885b72e8
wallwriting <hwang.john@gmail.com>
2023-03-23 04:58:08 -0700

    Create random_datetime.sql

---
+++ BigQuery/random_data_generator/random_datetime.sql
@@ -0,0 +1,10 @@
+/*creates random datetime down to microseconds*/
+CREATE OR REPLACE function demo.random_datetime(low timestamp ,high timestamp)
+as
+(
+        SELECT CAST(
+                    timestamp_micros(CAST(rand() * (UNIX_micros(high) - UNIX_micros(low)) + UNIX_micros(low) as int64))
+                    AS datetime
+                    )       
+    )
+;
