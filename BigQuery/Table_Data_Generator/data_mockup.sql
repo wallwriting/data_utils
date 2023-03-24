84fbe3390144eb2f0ba494ee13b0e3715e81ae7b
wallwriting <hwang.john@gmail.com>
2023-03-23 22:32:32 -0700

    Update data_mockup.sql
    Added section to exclude any fields that are not null

--- BigQuery/Table_Data_Generator/data_mockup.sql
+++ BigQuery/Table_Data_Generator/data_mockup.sql
@@ -10,7 +10,8 @@ DECLARE varRandomTimestamp STRING;
 DECLARE varRandomInt STRING;
 DECLARE varRandomString STRING;
 DECLARE varCurFunction STRING;
-
+DECLARE varMaxColArray STRING;
+DECLARE varGroomedColArray STRING;
 
 --DECLARE varLoadTbl STRING;
 --DECLARE varLoadPrj STRING;
@@ -32,6 +33,10 @@ SET varRandomInt = """test.random_integer(-1000000000, 1000000000)""";
 SET varRandomString = """test.random_string(250)""";
 
 
+/******************************************************************************/
+/*************************Loads data for the key column************************/
+/******************************************************************************/
+
 /*Does the initial load of the key column--the number of rows is determined by the arguments passed*/
 EXECUTE IMMEDIATE
 'INSERT INTO ' || varLoadDs || '.' || varLoadTbl
@@ -39,6 +44,123 @@ EXECUTE IMMEDIATE
 || 'SELECT sequence_number FROM test.meta_series WHERE sequence_number BETWEEN ' || varLowKeyVal || ' AND ' || varHighKeyVal ||';'
 ;
 
+/************************END SECTION*******************************************/
+
+
+
+
+
+
+/*******************************************************************************/
+/*********Gets list of not null columns to exclude from data load***************/
+/*******************************************************************************/
+    /*Creates a list of columns that are not NULL*/
+    EXECUTE IMMEDIATE 
+    'CREATE OR REPLACE TEMP TABLE tmp_ifnull_column_list_array AS '
+    || ' SELECT '
+    || '   REPLACE '
+    || '        ( '
+    || '            REPLACE'
+    || '                ( '
+    || '                    REPLACE'
+    || '                        ( '
+    || """                            (format('%T', array_agg(column_name))) """
+    || """                          , '''["''' , 'IFNULL(CAST(MAX(' """
+    || '                        ) '
+    || """                  , '''"]''', ''') AS STRING), 'F')''' """
+    || '                ) '
+    || """          , '''", "''', ''') AS STRING), 'F'), IFNULL(CAST(MAX(''' """
+    || '        ) AS col_array '
+    || ' FROM '
+    || '    ( '
+    || '        SELECT '
+    || '            column_name '
+    || '        FROM '
+    ||            varLoadPrj || '.' || varLoadDs || '.INFORMATION_SCHEMA.COLUMNS '
+    || '       WHERE '
+    || """            table_name = '""" || varLoadTbl || """' """
+     || '        ORDER BY '
+    || '            ordinal_position '
+    || '    ) '
+    || ' ;'
+    ;
+    SET varMaxColArray = (SELECT * FROM tmp_ifnull_column_list_array);
+
+    /*Creates a list of column names and removes any that are in the null list from the previous step*/
+    EXECUTE IMMEDIATE 
+    'CREATE OR REPLACE TEMP TABLE tmp_groomed_column_list_array AS '
+    || ' SELECT '
+    || '    /*filters out the opening and closing brackets as well as the double quotes from the        '
+    || '    array string in order to use it as an explicity column list in the query later on*/          '
+    || '    REPLACE                                                                                         '
+    || '        (                                                                                           '
+    || '            REPLACE                                                                                     '
+    || '                (                                                                                       '
+    || '                    REPLACE                                                                             '
+    || '                        (                                                                               '
+    || '                            REPLACE                                                                     '
+    || '                                (                                                                       '
+    || """                                    (FORMAT('%T', ARRAY_AGG(col1)))                                 """
+    || """                                  , '[', ''                                                         """
+    || '                                )                                                                       '
+    || """                          , ']', ''                                                                 """
+    || '                        )                                                                               '
+    || """            , 'xxxDELETETHISxxx, ', ''                                                              """
+    || '            )                                                                                           '
+    || """        , 'xxxDELETETHISxxx', ''                                                                    """
+    || '        ) AS col_array                                                                                   '
+    || ' FROM ' 
+    || '    ( '
+    || '        SELECT '
+    || """            (CASE WHEN col2 = 'F' THEN 'xxxDELETETHISxxx' """
+    || '                ELSE col1 ' 
+    || '                END) as col1 '
+    || '        FROM '
+    || '            ( '
+    || '                SELECT '
+    || '                    ROW_NUMBER() OVER() as key, ' 
+    || '                   * '
+    || '                FROM '
+    || '                UNNEST '
+    || '                    (( '
+    || '                    SELECT '
+    || '                        ARRAY_AGG(column_name) '
+    || '                    FROM '
+    || '                    ( '
+    || '                        SELECT '
+    || '                            column_name '
+    || '                        FROM '
+    ||                            varLoadPrj || '.' || varLoadDs || '.INFORMATION_SCHEMA.COLUMNS '
+    || '                       WHERE '
+    || """                            table_name = '""" || varLoadTbl || """' """
+    || '                        ORDER BY '
+    || '                            ordinal_position '
+    || '                    ) '
+    || '                    )) col1 '
+    || '            ) a '
+    || '        JOIN '
+    || '            ( '
+    || '                SELECT ' 
+    || '                    ROW_NUMBER() OVER() as key, ' 
+    || '                    * '
+    || '                FROM  '
+    || '                    UNNEST '
+    || '                        (( '
+    || '                            SELECT ' 
+    || '                                [ '
+    ||                                varMaxColArray
+    || '                                ] '
+    || '                            FROM ' || varLoadPrj || '.' || varLoadDs || '.' || varLoadTbl
+    || '                        )) col2 '
+    || '        ) b '
+    || '        ON a.key = b.key '
+    || '    ) '
+    || ' ; '
+    ;
+
+SET varGroomedColArray = (SELECT CASE WHEN TRIM(IFNULL(col_array, '')) = '' THEN '1 AS no_non_null_columns' ELSE col_array END FROM tmp_groomed_column_list_array);
+
+/***********************************END SECTION******************************************/
 
 /*creates a temp table that lists all columns for the table*/
 CREATE TEMP TABLE tmp_column_tracker (num_id   INT64, column_name   STRING, data_type   STRING);
@@ -54,7 +176,7 @@ EXECUTE IMMEDIATE
 ||     varLoadPrj || '.' || varLoadDs || '.INFORMATION_SCHEMA.COLUMNS ' 
 || 'WHERE '
 ||     'table_name = ' || """'""" || varLoadTbl || """' """
-||     'AND column_name != ' || """'""" || varKeyCol || """' """
+||     'AND column_name NOT IN( ' || varGroomedColArray || ')'
 ||     'ORDER BY ordinal_position'
 ||     ';' 
 ;
