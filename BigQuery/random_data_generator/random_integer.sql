74e85b5bbf38fca6bd7cce62224a639b928fac94
wallwriting <hwang.john@gmail.com>
2025-10-15 15:03:29 -0400

    Update random_integer.sql
    changed dataset

--- BigQuery/random_data_generator/random_integer.sql
+++ BigQuery/random_data_generator/random_integer.sql
@@ -1,5 +1,5 @@
 /*creates random integer*/
-CREATE OR REPLACE function demo.random_integer(low int64 ,high int64)
+CREATE OR REPLACE function dpa.random_integer(low int64 ,high int64)
 as
 ((select cast(rand() * (high - low) + low as int64)))
 ;
