634a707773cdd4f3c2804b6d216d17f5a8276a09
wallwriting <hwang.john@gmail.com>
2025-10-17 22:21:52 -0400

    Update random_numeric.sql
    Casts float to a numeric

--- BigQuery/random_data_generator/random_numeric.sql
+++ BigQuery/random_data_generator/random_numeric.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE function dpa.random_numeric(low numeric ,high numeric)
 as
 (
     (
-        select rand() * (high - low) + low
+        select CAST((rand() * (high - low) + low) AS NUMERIC)
     )
 )
 ;
