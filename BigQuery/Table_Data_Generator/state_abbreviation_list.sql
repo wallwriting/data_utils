0f8aa8ad78eef98f8d8413dc54e2334f6f80a052
wallwriting <hwang.john@gmail.com>
2025-10-20 01:15:36 -0400

    Create state_abbreviation_list.sql
    first version

---
+++ BigQuery/Table_Data_Generator/state_abbreviation_list.sql
@@ -0,0 +1,51 @@
+CREATE TABLE dpa.state_abbreviation_list AS
+  SELECT 'AL' AS state_abbrev, 'Alabama' as state_name UNION ALL 
+  SELECT 'AK', 'Alaska' UNION ALL 
+  SELECT 'AZ', 'Arizona' UNION ALL 
+  SELECT 'AR', 'Arkansas' UNION ALL 
+  SELECT 'CA', 'California' UNION ALL 
+  SELECT 'CO', 'Colorado' UNION ALL 
+  SELECT 'CT', 'Connecticut' UNION ALL 
+  SELECT 'DE', 'Delaware' UNION ALL 
+  SELECT 'FL', 'Florida' UNION ALL 
+  SELECT 'GA', 'Georgia' UNION ALL 
+  SELECT 'HI', 'Hawaii' UNION ALL 
+  SELECT 'ID', 'Idaho' UNION ALL 
+  SELECT 'IL', 'Illinois' UNION ALL 
+  SELECT 'IN', 'Indiana' UNION ALL 
+  SELECT 'IA', 'Iowa' UNION ALL 
+  SELECT 'KS', 'Kansas' UNION ALL 
+  SELECT 'KY', 'Kentucky' UNION ALL 
+  SELECT 'LA', 'Louisiana' UNION ALL 
+  SELECT 'ME', 'Maine' UNION ALL 
+  SELECT 'MD', 'Maryland' UNION ALL 
+  SELECT 'MA', 'Massachusetts' UNION ALL 
+  SELECT 'MI', 'Michigan' UNION ALL 
+  SELECT 'MN', 'Minnesota' UNION ALL 
+  SELECT 'MS', 'Mississippi' UNION ALL 
+  SELECT 'MO', 'Missouri' UNION ALL 
+  SELECT 'MT', 'Montana' UNION ALL 
+  SELECT 'NE', 'Nebraska' UNION ALL 
+  SELECT 'NV', 'Nevada' UNION ALL 
+  SELECT 'NH', 'New Hampshire' UNION ALL 
+  SELECT 'NJ', 'New Jersey' UNION ALL 
+  SELECT 'NM', 'New Mexico' UNION ALL 
+  SELECT 'NY', 'New York' UNION ALL 
+  SELECT 'NC', 'North Carolina' UNION ALL 
+  SELECT 'ND', 'North Dakota' UNION ALL 
+  SELECT 'OH', 'Ohio' UNION ALL 
+  SELECT 'OK', 'Oklahoma' UNION ALL 
+  SELECT 'OR', 'Oregon' UNION ALL 
+  SELECT 'PA', 'Pennsylvania' UNION ALL 
+  SELECT 'RI', 'Rhode Island' UNION ALL 
+  SELECT 'SC', 'South Carolina' UNION ALL 
+  SELECT 'SD', 'South Dakota' UNION ALL 
+  SELECT 'TN', 'Tennessee' UNION ALL 
+  SELECT 'TX', 'Texas' UNION ALL 
+  SELECT 'UT', 'Utah' UNION ALL 
+  SELECT 'VT', 'Vermont' UNION ALL 
+  SELECT 'VA', 'Virginia' UNION ALL 
+  SELECT 'WA', 'Washington' UNION ALL 
+  SELECT 'WV', 'West Virginia' UNION ALL 
+  SELECT 'WI', 'Wisconsin' UNION ALL 
+  SELECT 'WY', 'Wyoming'
